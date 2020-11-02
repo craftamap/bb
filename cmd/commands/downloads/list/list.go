ed49ffd19f8044e863718435403082908ea91134
Fabian Siegel <fabians1999@gmail.com>
2020-11-03 00:26:59 +0100

    Added Web for downloads

--- cmd/commands/downloads/list/list.go
+++ cmd/commands/downloads/list/list.go
@@ -8,7 +8,12 @@ import (
 	"github.com/craftamap/bb/internal"
 	"github.com/dustin/go-humanize"
 	"github.com/logrusorgru/aurora"
+	"github.com/pkg/browser"
 	"github.com/spf13/cobra"
+)
+
+var (
+	Web bool
 )
 
 func Add(downloadsCmd *cobra.Command, globalOpts *options.GlobalOptions) {
@@ -27,6 +32,14 @@ func Add(downloadsCmd *cobra.Command, globalOpts *options.GlobalOptions) {
 			}
 			if !bbrepo.IsBitbucketOrg() {
 				fmt.Printf("%s%s%s\n", aurora.Yellow(":: "), aurora.Bold("Warning: "), "Are you sure this is a bitbucket repo?")
+				return
+			}
+			if Web {
+				err := browser.OpenURL(fmt.Sprintf("https://bitbucket.org/%s/%s/downloads", bbrepo.RepoOrga, bbrepo.RepoSlug))
+				if err != nil {
+					fmt.Printf("%s%s%s\n", aurora.Red(":: "), aurora.Bold("An error occured: "), err)
+					return
+				}
 				return
 			}
 			downloads, err := c.GetDownloads(bbrepo.RepoOrga, bbrepo.RepoSlug)
@@ -48,6 +61,8 @@ func Add(downloadsCmd *cobra.Command, globalOpts *options.GlobalOptions) {
 			}
 		},
 	}
+
+	listCmd.Flags().BoolVar(&Web, "web", false, "show the downloads in your browsers")
 
 	downloadsCmd.AddCommand(listCmd)
 }
