347d6403cb4ca8f72c078edb963cffe009d031b9
Fabian Siegel <fabians1999@gmail.com>
2020-11-27 09:37:59 +0100

    Fixed target location

--- cmd/commands/downloads/download/download.go
+++ cmd/commands/downloads/download/download.go
@@ -1,10 +1,13 @@
 package download
 
 import (
+	"crypto/rand"
 	"fmt"
 	"io"
+	"math/big"
 	"net/http"
 	"os"
+	"path/filepath"
 	"strings"
 
 	"github.com/craftamap/bb/cmd/options"
@@ -26,10 +29,65 @@ func Add(prCmd *cobra.Command, globalOpts *options.GlobalOptions) {
 			"RequiresRepository": "true",
 		},
 		Run: func(cmd *cobra.Command, args []string) {
-			// TODO: Get rid of this "list" workaround
+			var (
+				remoteName  string
+				storagePath string
+			)
 
+			if len(args) == 0 {
+				fmt.Printf("%s%s%s\n", aurora.Red(":: "), aurora.Bold("An error occurred: "), "No file selected")
+				return
+			}
+			if len(args) >= 1 {
+				remoteName = args[0]
+				storagePath = "."
+			}
+			if len(args) == 2 {
+				storagePath = args[1]
+			}
+			if len(args) > 2 {
+				fmt.Printf("%s%s%s\n", aurora.Red(":: "), aurora.Bold("An error occurred: "), "Too many arguments")
+				return
+			}
+
+			// First, check if dir exists and is dir. if not, fail.
+			// Then check if filename exists. If not, use it as filename
+			// Else, check if its file or dir
+			//   If file, use it as filename
+			//   Else, use storagePath as dir and remoteName as file
+
+			dir, fname := filepath.Split(storagePath)
+			if dir != "" {
+				info, err := os.Stat(dir)
+				if err != nil {
+					fmt.Printf("%s%s%s\n", aurora.Red(":: "), aurora.Bold("An error occurred: "), err)
+					return
+				}
+				if !info.IsDir() {
+					fmt.Printf("%s%s%s\n", aurora.Red(":: "), aurora.Bold("An error occurred: "), fmt.Sprintf("%s is not a directory", dir))
+				}
+			}
+
+			info, err := os.Stat(storagePath)
+			if !os.IsNotExist(err) && info.IsDir() {
+				dir = storagePath
+				_, fname = filepath.Split(remoteName)
+			}
+			storagePath = filepath.Join(dir, fname)
+
+			// Safe downloads collition-free
+			info, err = os.Stat(storagePath)
+			if !os.IsNotExist(err) && !info.IsDir() {
+				dir, file := filepath.Split(storagePath)
+				file += "." + RandomString(10)
+				storagePath = filepath.Join(dir, file)
+			}
+
+			// Actual logic
 			c := globalOpts.Client
 			bbrepo := globalOpts.BitbucketRepo
+
+			fmt.Printf("%s%s\n", aurora.Green(":: "), "Getting all downloads")
 
 			downloads, err := c.GetDownloads(bbrepo.RepoOrga, bbrepo.RepoSlug)
 			if err != nil {
@@ -38,7 +96,7 @@ func Add(prCmd *cobra.Command, globalOpts *options.GlobalOptions) {
 			}
 
 			downloadMap := downloadsToMap(downloads)
-			dwnld, ok := downloadMap[args[0]]
+			dwnld, ok := downloadMap[remoteName]
 			if !ok {
 				fmt.Printf("%s%s%s\n", aurora.Red(":: "), aurora.Bold("An error occurred: "), err)
 				return
@@ -62,9 +120,9 @@ func Add(prCmd *cobra.Command, globalOpts *options.GlobalOptions) {
 			defer resp.Body.Close()
 
 			fmt.Printf("%s%s\n", aurora.Green(":: "), "Downloaded!")
-			fmt.Printf("%s%s\n", aurora.Green(":: "), "Saving file to .")
+			fmt.Printf("%s%s\n", aurora.Green(":: "), fmt.Sprintf("Saving file to %s", storagePath))
 
-			out, err := os.Create("./" + args[0])
+			out, err := os.Create(storagePath)
 			if err != nil {
 				fmt.Printf("%s%s%s\n", aurora.Red(":: "), aurora.Bold("An error occurred: "), err)
 				return
@@ -90,4 +148,15 @@ func downloadsToMap(downloads *internal.Downloads) map[string]internal.Download 
 		downloadMap[dwnld.Name] = dwnld
 	}
 	return downloadMap
+}
+
+func RandomString(n int) string {
+	var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+
+	s := make([]rune, n)
+	for i := range s {
+		a, _ := rand.Int(rand.Reader, big.NewInt(int64(len(letters))))
+		s[i] = letters[a.Int64()]
+	}
+	return string(s)
 }
