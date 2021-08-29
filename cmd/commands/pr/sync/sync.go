d4df8f7b59da65377c7f7bda49105b56f5b1e1f4
Fabian Siegel <fabians1999@gmail.com>
2021-08-29 19:38:17 +0200

    sync: make sync method configurable through the configuration (#12)

--- cmd/commands/pr/sync/sync.go
+++ cmd/commands/pr/sync/sync.go
@@ -6,17 +6,24 @@ import (
 	"os/exec"
 
 	"github.com/cli/cli/git"
+	"github.com/cli/cli/utils"
 	"github.com/cli/safeexec"
 	"github.com/craftamap/bb/cmd/options"
 	"github.com/craftamap/bb/internal/run"
 	"github.com/craftamap/bb/util/logging"
 	"github.com/logrusorgru/aurora"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 var (
-	Rebase bool
+	Method string
 	Push   bool
+)
+
+const (
+	MethodOptionMerge  = "merge"
+	MethodOptionRebase = "Rebase"
 )
 
 func Add(prCmd *cobra.Command, globalOpts *options.GlobalOptions) {
@@ -27,6 +34,22 @@ func Add(prCmd *cobra.Command, globalOpts *options.GlobalOptions) {
 		Annotations: map[string]string{
 			"RequiresClient":     "true",
 			"RequiresRepository": "true",
+		},
+		PreRunE: func(cmd *cobra.Command, _ []string) error {
+			// In order to check if the method exists in the config, we need to check here
+			syncMethodIsSet := viper.IsSet("sync-method")
+			if !syncMethodIsSet {
+				logging.Note("You can configure your preferred way of syncing by running TODO")
+			}
+			if !cmd.Flags().Lookup("method").Changed {
+				Method = viper.GetString("sync-method")
+			}
+
+			if Method != MethodOptionRebase && Method != MethodOptionMerge {
+				logging.Error("\"%s\" is not a valid method (select one of these: %s, %s)", Method, MethodOptionRebase, MethodOptionMerge)
+				return fmt.Errorf("\"%s\" is not a valid method (select one of these: %s, %s)", Method, MethodOptionRebase, MethodOptionMerge)
+			}
+			return nil
 		},
 		Run: func(_ *cobra.Command, _ []string) {
 			var id int
@@ -57,12 +80,15 @@ func Add(prCmd *cobra.Command, globalOpts *options.GlobalOptions) {
 				logging.Error(err)
 				return
 			}
+			if ucc, err := git.UncommittedChangeCount(); err == nil && ucc > 0 {
+				logging.Warning(utils.Pluralize(ucc, "uncommitted change; This might lead to issues"))
+			}
 
 			remoteDestinationBranch := fmt.Sprintf("origin/%s", pr.Destination.Branch.Name)
 
 			var cmdQueue [][]string
 			cmdQueue = append(cmdQueue, []string{"git", "fetch", "origin", pr.Destination.Branch.Name})
-			if Rebase {
+			if Method == MethodOptionRebase {
 				cmdQueue = append(cmdQueue, []string{"git", "rebase", remoteDestinationBranch})
 			} else {
 				cmdQueue = append(cmdQueue, []string{"git", "merge", "--commit", remoteDestinationBranch})
@@ -73,7 +99,7 @@ func Add(prCmd *cobra.Command, globalOpts *options.GlobalOptions) {
 				logging.Error(err)
 				logging.Note("Looks like an error occurred - you probably need to resolve the conflict manually now. Start by running " + aurora.BgBrightBlack(aurora.White(" git status ")).String() + " to get hints on how to resolve the conflicts.")
 				cancelCommand := aurora.BgBrightBlack(aurora.White(" git merge --abort ")).String()
-				if Rebase {
+				if Method == MethodOptionRebase {
 					cancelCommand = aurora.BgBrightBlack(aurora.White(" git rebase --abort ")).String()
 				}
 				logging.Note("If you don't know how to resolve a conflict manually, you can run " + cancelCommand + " to reset.")
@@ -90,7 +116,7 @@ func Add(prCmd *cobra.Command, globalOpts *options.GlobalOptions) {
 		},
 	}
 
-	syncCmd.Flags().BoolVarP(&Rebase, "rebase", "r", false, "uses rebase instead of merge to sync")
+	syncCmd.Flags().StringVar(&Method, "method", "merge", "sync using merge or rebase (merge/rebase)")
 	syncCmd.Flags().BoolVar(&Push, "push", true, "push after merge/rebase")
 
 	prCmd.AddCommand(syncCmd)
